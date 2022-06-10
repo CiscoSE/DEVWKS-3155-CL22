acc197f6770709e7045861ae18961c5a2e412b6a
Tim Miller <timmil@cisco.com>
2022-06-10 15:52:07 -0400

    Add required environment variables and dcnm_lan_sdk python module

--- setup-python.sh
+++ setup-python.sh
@@ -12,3 +12,15 @@ pip install -r requirements.txt
 
 # Install Ansible required collections
 ansible-galaxy collection install -r requirements.yaml
+
+# Install Python utilities for NDFC
+pip install utils/dcnm_lan_fabric-0.2.4-py3-none-any.whl
+
+# Set up NDFCCTL connection credentials
+export NDFC_HOST=198.18.134.200
+export NDFC_USER=admin
+export NDFC_PASS=C1sco12345
+
+# Set up Ansible switch discovery credentials
+export SWITCH_USER=admin
+export SWITCH_PASS=C1sco12345
